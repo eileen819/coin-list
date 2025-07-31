b74b9f04857df9ca11579dfe58bc6fb5d9cf78e5
eileen819 <eileen.ju.8819@gmail.com>
2025-08-01 01:01:50 +0900

    다크모드 아이콘 위치 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const ModeBtn = styled.div<{ $isActive: boolean }>`
   transition: transform 0.3s ease-in;
 `;
 
-const ModeSunIcon = styled.div<{ $isActive: boolean }>`
+const ModeLight = styled.div<{ $isActive: boolean }>`
   position: absolute;
   top: 5px;
   right: 4px;
@@ -60,7 +60,7 @@ const ModeSunIcon = styled.div<{ $isActive: boolean }>`
   transition: opacity 0.3s ease-in, visibility 0.5s ease-in;
 `;
 
-const ModeMoonIcon = styled(ModeSunIcon)`
+const ModeDark = styled(ModeLight)`
   left: 4px;
   opacity: ${(props) => (props.$isActive ? 1 : 0)};
   visibility: ${(props) => (props.$isActive ? "visible" : "hidden")};
@@ -98,23 +98,7 @@ function Header() {
 
       <ModeTabs $isActive={isDark} onClick={onToggle}>
         <ModeBtn $isActive={isDark}></ModeBtn>
-        <ModeMoonIcon $isActive={isDark}>
-          <svg
-            fill="none"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-            />
-          </svg>
-        </ModeMoonIcon>
-        <ModeSunIcon $isActive={isDark}>
+        <ModeDark $isActive={isDark}>
           <svg
             fill="none"
             strokeWidth={1.5}
@@ -129,7 +113,23 @@ function Header() {
               d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
             />
           </svg>
-        </ModeSunIcon>
+        </ModeDark>
+        <ModeLight $isActive={isDark}>
+          <svg
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+            />
+          </svg>
+        </ModeLight>
       </ModeTabs>
     </Wrapper>
   );
